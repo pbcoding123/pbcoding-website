0feb76dbe934e34293088d714d83210a923c7c19
pbcoding123 <pbcoding123@163.com>
2026-04-26 21:52:46 +0800

    Update addDiv.js

--- passcode/addDiv.js
+++ passcode/addDiv.js
@@ -5,8 +5,8 @@ function addsecretdiv(){
 	newDiv.innerHTML = '\
             <div class="bg-white rounded-2xl p-8 card-hover" id="secretpass">\
             	<h3>SecretText</h3>\
-				<twisty-player alg="x2050 (R65 U65 R63 U63 R63 F65 B64 E64 D64 R66 L64 U63 R63 B64 M64 S64 U63 R65 U65 R63 M64 F63)2"></twisty-player>\
 				<script src="https://cdn.cubing.net/v0/js/cubing/twisty" type="module"></script>\
+				<twisty-player alg="x2050 (R65 U65 R63 U63 R63 F65 B64 E64 D64 R66 L64 U63 R63 B64 M64 S64 U63 R65 U65 R63 M64 F63)2"></twisty-player>\
             </div>';
 	newDiv.setAttribute('id', 'newDiv');
 
